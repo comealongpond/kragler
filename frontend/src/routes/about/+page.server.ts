648b4d87a6475232cd2adf265203e60ab9599ec6
comealongpond <erikgurk@hotmail.com>
2023-01-19 21:35:05 +0100

    forgor folder

--- frontend/src/routes/about/+page.server.ts
+++
@@ -1,8 +0,0 @@
-// import * as db from '$lib/server/database';
- 
-/** @type {import('./$types').PageServerLoad} */
-export async function load({ params }: { params: any }) {
-  return {
-    post: 'yeet'//await db.getPost(params.slug)
-  };
-}
